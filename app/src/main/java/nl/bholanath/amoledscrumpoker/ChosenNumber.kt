8b1a55503a58f3b3f71e75af751d40919ea12bf6
Radjino Bholanath <radjino.bholanath@gmail.com>
2017-06-28 22:23:14 +0200

    Animations

--- app/src/main/java/nl/bholanath/amoledscrumpoker/ChosenNumber.kt
+++ app/src/main/java/nl/bholanath/amoledscrumpoker/ChosenNumber.kt
@@ -3,11 +3,12 @@ package nl.bholanath.amoledscrumpoker
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import android.provider.AlarmClock.EXTRA_MESSAGE
-import android.content.Intent
 import android.graphics.Color
 import android.view.View
-import android.widget.EditText
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
+
+
 
 class ChosenNumber : AppCompatActivity()
 {
@@ -16,6 +17,7 @@ class ChosenNumber : AppCompatActivity()
         const val FONT_SIZE_MEDIUM = 350.0f
         const val FONT_SIZE_LARGE = 500.0f
         const val TEXT_COLOR = Color.WHITE
+        const val TEXT_COLOR_HIDDEN = Color.BLACK
     }
 
     override fun onCreate(savedInstanceState: Bundle?)
@@ -26,7 +28,7 @@ class ChosenNumber : AppCompatActivity()
         val intent = intent
         val message = intent.getStringExtra(MainActivity.CHOSEN_VALUE)
 
-        val textView = findViewById(R.id.editText) as TextView
+        val textView = findViewById(R.id.textView) as TextView
         textView.text = message
 
         when(message.length)
@@ -39,7 +41,20 @@ class ChosenNumber : AppCompatActivity()
 
     fun screenTapped(view: View?)
     {
-        val textView = findViewById(R.id.editText) as TextView
-        textView.setTextColor(ChosenNumber.TEXT_COLOR)
+        val textView = findViewById(R.id.textView) as TextView
+
+        if (textView.currentTextColor != ChosenNumber.TEXT_COLOR_HIDDEN)
+            return
+
+        val objectAnimator = ObjectAnimator.ofInt(textView, "textColor", ChosenNumber.TEXT_COLOR_HIDDEN, ChosenNumber.TEXT_COLOR)
+        objectAnimator.setEvaluator(ArgbEvaluator())
+        objectAnimator.setDuration(1000)
+        objectAnimator.start()
+
+        val textView2 = findViewById(R.id.textView2) as TextView
+        val objectAnimator2 = ObjectAnimator.ofInt(textView2, "textColor", ChosenNumber.TEXT_COLOR, ChosenNumber.TEXT_COLOR_HIDDEN)
+        objectAnimator2.setEvaluator(ArgbEvaluator())
+        objectAnimator2.setDuration(1000)
+        objectAnimator2.start()
     }
 }
