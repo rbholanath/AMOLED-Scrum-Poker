3bcb70bcf91987e85a732ff2735f27fe92e4ca61
Radjino Bholanath <radjino.bholanath@gmail.com>
2017-07-01 23:36:27 +0200

    Pinch to zoom

--- app/src/main/java/nl/bholanath/amoledscrumpoker/ChosenNumber.kt
+++ app/src/main/java/nl/bholanath/amoledscrumpoker/ChosenNumber.kt
@@ -7,52 +7,97 @@ import android.graphics.Color
 import android.view.View
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
+import android.view.ScaleGestureDetector
 import kotlinx.android.synthetic.main.activity_chosen_number.textView
 import kotlinx.android.synthetic.main.activity_chosen_number.textView2
 
 class ChosenNumber : AppCompatActivity()
 {
-    companion object {
-        const val FONT_SIZE_SMALL = 235.0f
-        const val FONT_SIZE_MEDIUM = 350.0f
-        const val FONT_SIZE_LARGE = 500.0f
+    companion object
+    {
+        const val FONT_SIZE_MINIMUM = 50f
+        const val FONT_SIZE_STANDARD = 150f
+        const val FONT_SIZE_SMALL = 235f
+        const val FONT_SIZE_MEDIUM = 350f
+        const val FONT_SIZE_LARGE = 500f
+
         const val TEXT_COLOR = Color.WHITE
         const val TEXT_COLOR_HIDDEN = Color.BLACK
     }
+
+    lateinit var scaleGestureDetector: ScaleGestureDetector
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chosen_number)
 
-        val intent = intent
-        val message = intent.getStringExtra(MainActivity.CHOSEN_VALUE)
+        textView.text = intent.getStringExtra(MainActivity.CHOSEN_VALUE)
 
-        val textView = findViewById(R.id.textView) as TextView
-        textView.text = message
+        textView.textSize = ChosenNumber.FONT_SIZE_STANDARD
 
-        when(message.length)
-        {
-            1 -> textView.textSize = ChosenNumber.FONT_SIZE_LARGE
-            2 -> textView.textSize = ChosenNumber.FONT_SIZE_MEDIUM
-            3 -> textView.textSize = ChosenNumber.FONT_SIZE_SMALL
-        }
+        setupPinchToZoom()
     }
 
     @Suppress("UNUSED_PARAMETER")
-    fun screenTapped(view: View?)
+    fun arrowTapped(view: View?)
     {
-        if (textView.currentTextColor != ChosenNumber.TEXT_COLOR_HIDDEN)
-            return
+        if (textView.currentTextColor == ChosenNumber.TEXT_COLOR_HIDDEN)
+        {
+            screenTapped()
+        }
+    }
 
+    private fun setupPinchToZoom() {
+        textView.setOnTouchListener { _, event ->
+            if (textView.currentTextColor == ChosenNumber.TEXT_COLOR_HIDDEN)
+            {
+                screenTapped()
+            }
+            else if (textView.currentTextColor == ChosenNumber.TEXT_COLOR)
+            {
+                scaleGestureDetector.onTouchEvent(event)
+            }
+
+            true
+        }
+
+        var scaleFactor = 1f
+
+        scaleGestureDetector = ScaleGestureDetector(this,
+                object : ScaleGestureDetector.SimpleOnScaleGestureListener()
+                {
+                    override fun onScale(detector: ScaleGestureDetector): Boolean
+                    {
+                        val maximumFontSize = when(textView.text.length)
+                        {
+                            1 -> ChosenNumber.FONT_SIZE_LARGE
+                            2 -> ChosenNumber.FONT_SIZE_MEDIUM
+                            3 -> ChosenNumber.FONT_SIZE_SMALL
+                            else -> ChosenNumber.FONT_SIZE_SMALL
+                        }
+
+                        scaleFactor *= detector.scaleFactor
+
+                        val textSizeInSp = textView.textSize / resources.displayMetrics.scaledDensity
+
+                        textView.textSize = Math.max(ChosenNumber.FONT_SIZE_MINIMUM, Math.min(maximumFontSize, textSizeInSp * scaleFactor))
+
+                        return true
+                    }
+                })
+    }
+
+    private fun screenTapped()
+    {
         val objectAnimator = ObjectAnimator.ofInt(textView, "textColor", ChosenNumber.TEXT_COLOR_HIDDEN, ChosenNumber.TEXT_COLOR)
         objectAnimator.setEvaluator(ArgbEvaluator())
-        objectAnimator.setDuration(1000)
+        objectAnimator.duration = 1000
         objectAnimator.start()
 
         val objectAnimator2 = ObjectAnimator.ofInt(textView2, "textColor", ChosenNumber.TEXT_COLOR, ChosenNumber.TEXT_COLOR_HIDDEN)
         objectAnimator2.setEvaluator(ArgbEvaluator())
-        objectAnimator2.setDuration(1000)
+        objectAnimator2.duration = 1000
         objectAnimator2.start()
     }
 }
--- app/src/main/java/nl/bholanath/amoledscrumpoker/MainActivity.kt
+++ app/src/main/java/nl/bholanath/amoledscrumpoker/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.content.Intent
-import android.support.design.widget.BottomNavigationView
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
